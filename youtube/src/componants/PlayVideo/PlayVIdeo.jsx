b6e077f431abb35bd98d2ad89ae890f32166dfa6
vishal birla <135350754+vishalbirla44@users.noreply.github.com>
2024-06-14 10:10:51 +0000

    2

--- youtube/src/componants/PlayVideo/PlayVIdeo.jsx
+++ youtube/src/componants/PlayVideo/PlayVIdeo.jsx
@@ -7,34 +7,35 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
-import { API_KEY } from '../../Data'
-const PlayVIdeo = ({videoId}) => {
+import { API_KEY, value_converter } from '../../Data'
+import moment from 'moment'
+const PlayVIdeo = ({ videoId }) => {
 
-const [apideta, setApideta] = useState(null)
- 
-const fatchVideoDeta = async () => {
-  const videoDteails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+  const [apideta, setApideta] = useState(null)
 
-  await fetch(videoDteails_url).then(response=>response.json()).then(data => setApideta(data.items[0]))
+  const fatchVideoDeta = async () => {
+    const videoDteails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
 
-
-}
-
-console.log(apideta)
+    await fetch(videoDteails_url).then(response => response.json()).then(data => setApideta(data.items[0]))
 
 
-useEffect(() => {
-fatchVideoDeta()
-},[])
+  }
+
+  
+
+
+  useEffect(() => {
+    fatchVideoDeta()
+  }, [])
 
   return (
     <div className='play-video'>
       {/* <video src={video1} controls autoPlay muted></video> */}
-      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-      <h3>{apideta?apideta.snippet.title:'Title here'}</h3>
+      <h3>{apideta ? apideta.snippet.title : 'Title here'}</h3>
       <div className='play_video_info'>
-        <p>145 Views &bull; 2 days ago </p>
+        <p>{apideta ? value_converter(apideta.statistics.viewCount) : "16K"} &bull; {moment(apideta.snippet.publishedAt).FromNow()} </p>
         <div>
           <span><img src={like} alt="" />123</span>
           <span><img src={dislike} alt="" />2</span>
@@ -42,7 +43,7 @@ fatchVideoDeta()
           <span><img src={save} alt="" />Save</span>
         </div>
       </div>
-      <hr/>
+      <hr />
       <div className='publisher'>
         <img src={jack} alt="" />
         <div>
@@ -55,7 +56,7 @@ fatchVideoDeta()
       <div className='vid-description'>
         <p>chanel thats essy to learn</p>
         <p> subscribe this chanel </p>
-        <hr/>
+        <hr />
         <h4>130 Comments</h4>
         <div className='comment'>
           <img src={user_profile} alt="" />
